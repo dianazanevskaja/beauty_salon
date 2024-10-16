d2b7f8735d743d896ac00b404b13c923b2ba9ccf
diana <dianazanevskaja@gmail.com>
2024-10-16 23:53:47 +0300

    Update AboutPage.js and create About.css

--- src/pages/About/AboutPage.js
+++ src/pages/About/AboutPage.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import '../../styles/About.css';
+
+const ContactPage = () => {
+  return (
+    <div className="container">
+      <h1>Beauty salon contacts</h1>
+      <div className="contact-info">
+        <p><strong>Адрес:</strong> T. Grodno, st. Suvorova, 308 </p>
+        <p><strong>Телефон:</strong> +375 (33) 333-33-33</p>
+        <p><strong>Email:</strong> beauty@gmail.com</p>
+      </div>
+    </div>
+  );
+};
 
 const AboutPage = () => {
   return (
-    <h1>AboutPage</h1>
-  )
-}
+    <ContactPage />
+  );
+};
 
-export default AboutPage
+export default AboutPage;
