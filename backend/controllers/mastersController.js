5747b078887bcb326130d4fcb2acc597c5ed3b63
diana <dianazanevskaja@gmail.com>
2024-12-04 02:13:06 +0300

    feat: update mastersController

--- backend/controllers/mastersController.js
+++ backend/controllers/mastersController.js
@@ -1,7 +1,10 @@
 const db = require('../config/db');
 
 function getAllMasters(req, res) {
-  const mastersDataQuery = `SELECT * FROM master`;
+  const mastersDataQuery = `SELECT mw.id, u.id AS master_id, u.firstName, u.lastName, u.birthday, mw.workexp_id as coefficient_id, we.coefficient
+                            FROM users AS u 
+                            JOIN master_workexpirience AS mw ON u.id = mw.master_id
+                            JOIN workexpirience AS we ON mw.workexp_id = we.id;`;
   db.query(mastersDataQuery, (err, results) => {
     if (err) {
       console.error(err);
@@ -12,63 +15,59 @@ function getAllMasters(req, res) {
 }
 
 function createMaster(req, res) {
-  const { firstName, lastName, coefficient } = req.body;
-  const insertMasterQuery = `INSERT INTO master (firstName, lastName, coefficient) VALUES (?, ?, ?)`;
-  const values = [firstName, lastName, coefficient];
-  db.query(insertMasterQuery, values, (err, result) => {
+  const { master_id, coefficient_id } = req.body;
+  const insertMasterWorkExpQuery = `INSERT INTO master_workexpirience (master_id, workexp_id) VALUES (?, ?)`;
+
+  db.query(insertMasterWorkExpQuery, [master_id, coefficient_id], (err, result) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ message: 'Error adding master' });
-    }
-    res.status(201).json({ id: result.insertId, message: 'Master added successfully' });
-  });
-}
-
-function deleteMaster(req, res) {
-  const id = req.params.id;
-  const deleteMasterServicesQuery = 'DELETE FROM master_services WHERE master_id = ?';
-  const deleteMasterQuery = 'DELETE FROM master WHERE id = ?';
-
-  db.query(deleteMasterServicesQuery, [id], (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: 'Error deleting associated master services' });
+      return res.status(500).json({ message: 'Error adding master work experience' });
     }
 
-    db.query(deleteMasterQuery, [id], (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ message: 'Error deleting master' });
-      }
-
-      res.status(200).json({ message: 'Master deleted successfully' });
-    });
+    res.status(201).json({ id: result.insertId, message: 'Master work experience added successfully' });
   });
 }
 
 function updateMaster(req, res) {
-  const id = req.params.id;
-  const { firstName, lastName, coefficient } = req.body;
-  const updateMasterQuery = 'UPDATE master SET firstName=?, lastName=?, coefficient=? WHERE id=?';
-  const values = [firstName, lastName, coefficient, id];
-  db.query(updateMasterQuery, values, (err) => {
+  const masterExpId = req.params.id;
+  const { master_id, coefficient_id } = req.body;
+  const updateMasterWorkExpQuery = `UPDATE master_workexpirience SET master_id = ?, workexp_id = ? WHERE id = ?`;
+
+  db.query(updateMasterWorkExpQuery, [master_id, coefficient_id, masterExpId], (err, result) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ message: 'Error updating master' });
+      return res.status(500).json({ message: 'Error updating master work experience' });
     }
-    res.status(200).json({ message: 'Master updated successfully' });
+
+    res.status(200).json({ message: 'Master work experience updated successfully' });
+  });
+}
+
+function deleteMaster(req, res) {
+  const masterId = req.params.id;
+  const deleteMasterWorkExpQuery = 'DELETE FROM master_workexpirience WHERE id = ?';
+
+  db.query(deleteMasterWorkExpQuery, [masterId], (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: 'Error deleting master work experience' });
+    }
+
+    res.status(200).json({ message: 'Master work experience deleted successfully' });
   });
 }
 
 function getMastersData(req, res) {
   const mastersDataQuery = `
-    SELECT m.id, m.firstName, m.lastName, m.coefficient, 
-    JSON_ARRAYAGG(JSON_OBJECT('name', s.name, 'category', c.category)) AS services
-    FROM master_services ms
-    INNER JOIN master m ON ms.master_id = m.id
-    INNER JOIN services s ON ms.service_id = s.id
-    INNER JOIN category c ON s.category_id = c.id
-    GROUP BY m.id;
+  SELECT u.id, u.firstName, u.lastName, w.coefficient,
+  JSON_ARRAYAGG(JSON_OBJECT('name', s.name, 'category', c.category)) AS services
+  FROM master_services ms
+  INNER JOIN master_workexpirience mw ON ms.master_workexpirience_id = mw.id
+  INNER JOIN users u ON mw.master_id = u.id
+  INNER JOIN services s ON ms.service_id = s.id
+  INNER JOIN category c ON s.category_id = c.id
+  INNER JOIN workexpirience w ON mw.workexp_id = w.id
+  GROUP BY u.id, u.firstName, u.lastName, w.coefficient;
   `;
   db.query(mastersDataQuery, (err, results) => {
     if (err) {
