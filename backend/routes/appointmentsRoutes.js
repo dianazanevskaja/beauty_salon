b1aad8465d193f125583d47b22e5a6bdfb0caaff
diana <dianazanevskaja@gmail.com>
2024-12-04 02:12:13 +0300

    feat: update appointmensRoutes

--- backend/routes/appointmentsRoutes.js
+++ backend/routes/appointmentsRoutes.js
@@ -4,13 +4,15 @@ const db = require('../config/db');
 
 router.get('/', (req, res) => {
   const query = `
-    SELECT a.id, c.email, c.firstName, c.lastName, a.date_signup, a.time_signup, m.firstName as masterFirstName, m.lastName as masterLastName, s.name as serviceName, s.price * m.coefficient as price
-    FROM client_master_services a
-    INNER JOIN client c ON a.client_id = c.id
-    INNER JOIN master_services ms ON a.master_service_id = ms.id
-    INNER JOIN master m ON ms.master_id = m.id
-    INNER JOIN services s ON ms.service_id = s.id
-    ORDER BY a.date_signup DESC, a.time_signup ASC;
+  SELECT a.id, u.email, u.firstName, u.lastName, a.date_signup, a.time_signup, m.firstName as masterFirstName, m.lastName as masterLastName, s.name as serviceName, s.price * we.coefficient as price
+  FROM client_master_services a
+  INNER JOIN users u ON a.client_id = u.id
+  INNER JOIN master_services ms ON a.master_service_id = ms.id
+  INNER JOIN master_workexpirience mw ON ms.master_workexpirience_id = mw.id
+  INNER JOIN services s ON ms.service_id = s.id
+  INNER JOIN workexpirience we ON mw.workexp_id = we.id
+  INNER JOIN users m ON mw.master_id = m.id
+  ORDER BY a.date_signup DESC, a.time_signup ASC;
   `;
 
   db.query(query, (err, results) => {
